64c65be5e1d23edfe352b79f868c55d4913066e7
cvalingam <cvalingam-r@hotmail.com>
2025-02-01 16:50:11 +0530

    feat: DCC 01-02-2025 added.

---
+++ solutions/3151. Special Array I/3151.cs
@@ -0,0 +1,12 @@
+public class Solution
+{
+    public bool IsArraySpecial(int[] nums)
+    {
+        for (int i = 1; i < nums.Length; ++i)
+        {
+            if (nums[i] % 2 == nums[i - 1] % 2)
+                return false;
+        }
+        return true;
+    }
+}
