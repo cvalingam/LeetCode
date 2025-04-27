2690e965c440a01c001afa5e3ea22860f2ffec13
cvalingam <cvalingam-r@hotmail.com>
2025-04-27 08:27:59 +0530

    feat: DCC 27-04-2025 added

---
+++ solutions/3392. Count Subarrays of Length Three With a Condition/3392.cs
@@ -0,0 +1,15 @@
+public class Solution
+{
+    public int CountSubarrays(int[] nums)
+    {
+        int ans = 0;
+
+        for (int i = 1; i + 1 < nums.Length; ++i)
+        {
+            if (nums[i] == (nums[i - 1] + nums[i + 1]) * 2)
+                ++ans;
+        }
+
+        return ans;
+    }
+}
