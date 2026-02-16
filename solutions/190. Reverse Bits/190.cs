3e683fd2f589a9ee5f0094dd9cd9d475f6542171
cvalingam <cvalingam-r@hotmail.com>
2026-02-16 11:23:59 +0530

    feat: DCC 16-02-2026 added

--- solutions/190. Reverse Bits/190.cs
+++ solutions/190. Reverse Bits/190.cs
@@ -1,8 +1,26 @@
 public class Solution
 {
-    public uint reverseBits(uint n)
+    public uint ReverseBits(uint n)
     {
         uint ans = 0;
+
+        for (int i = 0; i < 32; i++)
+        {
+            ans <<= 1;
+            if ((n & 1) == 1)
+                ans += 1;
+            n >>= 1;
+        }
+
+        return ans;
+    }
+}
+
+public class Solution1
+{
+    public int ReverseBits(int n)
+    {
+        int ans = 0;
 
         for (int i = 0; i < 32; i++)
         {
