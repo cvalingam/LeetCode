0c0922d46d03b3a532940603d5d02d523647ba21
cvalingam <cvalingam-r@hotmail.com>
2026-02-18 12:31:11 +0530

    feat: DCC 18-02-2026 added

---
+++ solutions/693. Binary Number with Alternating Bits/693.cs
@@ -0,0 +1,13 @@
+public class Solution
+{
+    public bool HasAlternatingBits(int n)
+    {
+        //            n = 0b010101
+        //       n >> 2 = 0b000101
+        // n ^ (n >> 2) = 0b010000 = a
+        //        a - 1 = 0b001111
+        //  a & (a - 1) = 0
+        int a = n ^ (n >> 2);
+        return (a & (a - 1)) == 0;
+    }
+}
