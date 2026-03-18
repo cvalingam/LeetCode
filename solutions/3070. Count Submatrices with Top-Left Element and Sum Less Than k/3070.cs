426b1e41c3165d61762a3e5e4b97fad066c318b0
Sivalingam <cvalingam-r@hotmail.com>
2026-03-18 06:49:25 +0530

    feat: DCC 18-03-2026 added

---
+++ solutions/3070. Count Submatrices with Top-Left Element and Sum Less Than k/3070.cs
@@ -0,0 +1,22 @@
+public class Solution
+{
+    public int CountSubmatrices(int[][] grid, int k)
+    {
+        int m = grid.Length;
+        int n = grid[0].Length;
+        int ans = 0;
+        int[,] prefix = new int[m + 1, n + 1];
+
+        for (int i = 0; i < m; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                prefix[i + 1, j + 1] = grid[i][j] + prefix[i, j + 1] + prefix[i + 1, j] - prefix[i, j];
+                if (prefix[i + 1, j + 1] <= k)
+                    ++ans;
+            }
+        }
+
+        return ans;
+    }
+}
