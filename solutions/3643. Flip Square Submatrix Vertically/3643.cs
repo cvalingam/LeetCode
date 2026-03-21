19fd0cee5143dbd54e7ce7b9f2466efddf7a6c12
Sivalingam <cvalingam-r@hotmail.com>
2026-03-21 12:40:12 +0530

    feat: DCC 21-03-2026 added

---
+++ solutions/3643. Flip Square Submatrix Vertically/3643.cs
@@ -0,0 +1,23 @@
+public class Solution
+{
+    public int[][] ReverseSubmatrix(int[][] grid, int x, int y, int k)
+    {
+        // Iterate through the first half of the rows in the submatrix
+        for (int row = x; row < x + k / 2; row++)
+        {
+            // Calculate the corresponding row from the bottom to swap with
+            int mirrorRow = x + k - 1 - (row - x);
+
+            // Swap all elements in the current row with the mirror row
+            for (int col = y; col < y + k; col++)
+            {
+                // Perform the swap using a temporary variable
+                int temp = grid[row][col];
+                grid[row][col] = grid[mirrorRow][col];
+                grid[mirrorRow][col] = temp;
+            }
+        }
+
+        return grid;
+    }
+}
