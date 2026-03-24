36cd02da4df3f857c46cfd576a4e80d4ad8f87d3
Sivalingam <cvalingam-r@hotmail.com>
2026-03-24 11:46:00 +0530

    feat: add GFG solution for Course Schedule I

---
+++ gfg-solutions/Course Schedule I/Course Schedule I.java
@@ -0,0 +1,45 @@
+
+import java.util.*;
+
+class Solution {
+
+    public boolean canFinish(int n, int[][] prerequisites) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        int[] indegree = new int[n];
+
+        for (int[] pre : prerequisites) {
+            int course = pre[0];
+            int prereq = pre[1];
+
+            adj.get(prereq).add(course);
+            indegree[course]++;
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0) {
+                q.offer(i);
+            }
+        }
+
+        int count = 0;
+
+        while (!q.isEmpty()) {
+            int curr = q.poll();
+            count++;
+            for (int neighbor : adj.get(curr)) {
+                indegree[neighbor]--;
+
+                if (indegree[neighbor] == 0) {
+                    q.offer(neighbor);
+                }
+            }
+        }
+
+        return count == n;
+    }
+}
