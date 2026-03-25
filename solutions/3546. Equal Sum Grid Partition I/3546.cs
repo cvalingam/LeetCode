f1fac1b1af91b60f47ff410c8f16608f783a5a9e
Sivalingam <cvalingam-r@hotmail.com>
2026-03-25 12:33:40 +0530

    solutions: LeetCode 3546 (Equal Sum Grid Partition I) + GFG POTD (Minimum Height Roots)

---
+++ solutions/3546. Equal Sum Grid Partition I/3546.cs
@@ -0,0 +1,36 @@
+public class Solution
+{
+    public bool CanPartitionGrid(int[][] grid)
+    {
+        long totalSum = grid.SelectMany(row => row).Sum(x => (long)x);
+        return CanPartition(grid, totalSum) || CanPartition(Transposed(grid), totalSum);
+    }
+
+    private bool CanPartition(int[][] lines, long totalSum)
+    {
+        long runningSum = 0;
+        foreach (var line in lines)
+        {
+            runningSum += line.Sum(x => (long)x);
+            if (runningSum * 2 == totalSum)
+                return true;
+        }
+        
+        return false;
+    }
+
+    private int[][] Transposed(int[][] grid)
+    {
+        int rows = grid.Length;
+        int cols = grid[0].Length;
+        int[][] res = new int[cols][];
+        for (int j = 0; j < cols; j++)
+        {
+            res[j] = new int[rows];
+            for (int i = 0; i < rows; i++)
+                res[j][i] = grid[i][j];
+        }
+
+        return res;
+    }
+}
